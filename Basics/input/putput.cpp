e77ae2d4ea3e2f7d3f92d3da847dcda3a1cae550
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-23 14:58:38 +0530

    i/p o/p

---
+++ Basics/input/putput.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+
+// int main(){
+//     std::cout <<"hey Ayush"<<"\n";
+//     std::cout <<"hey Striver "<<std::endl<<"hey ayush";
+// }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello sahab ji";
+
+}
