d6bc46af3af6df104f16f72c8b6651844fc4bf35
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-23 15:07:25 +0530

    data types

---
+++ Basics/datatypes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int
+    int x= 10;
+    cout<<x;
+    // ranges -21478348 to 21478347
+    // beyond this long will help you 
+
+    // long 
+    long  xy=22322;
+    cout<<xy;
+
+    long long xz =224424233232;
+    cout<<xz;
+
+    float w = 4.5;
+    double t = 33.555;
+
+return 0;
+}
--- Basics/input/output.cpp
+++ Basics/input/output.cpp
@@ -12,4 +12,11 @@ using namespace std;
 int main(){
     cout<<"hello sahab ji";
 
+
+    int x,y;
+    cin>>x>>y;
+    cout<<"valueof x: "<<x<<"value of y is :"<<y;
+
+    // o/p---> valueof x: 33value of y is :33
+    
 }
