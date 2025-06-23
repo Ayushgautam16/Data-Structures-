c39d2d2b3f6f4a48c1317d8895277d8ba27c68c7
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-23 15:15:27 +0530

    getline

---
+++ Basics/string_getline.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+// string s1 ;
+// string s2 ;
+// cin>> s1>>s2;
+// cout<<s1<<" "<<s2;
+// return 0;
+
+// string str ;
+// getline(cin, str);
+// cout<<"kya bol rahey the "<<str;
+// return 0;
+
+// o/p ---->kya bol rahey the hey buddy how it going 
+
+char ch;
+cin >> ch;
+cout<<ch;
+
+
+// o/p -----> re
+// r
+
+// takes only single char 
+
+return 0;
+}
