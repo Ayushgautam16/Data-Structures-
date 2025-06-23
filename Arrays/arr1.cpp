a4ae19a0e7244e5a76cea09bbecf370fcdb9ea68
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-23 16:18:52 +0530

    1D Array

---
+++ Arrays/arr1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[5];
+cin >>arr[0]>>arr[1] >>arr[2] >>arr[3] >>arr[4] >>arr[5];
+
+cout<<"values ye hai ";
+
+cout <<arr[0]<<" "<<arr[1]<<" " <<arr[2]<<" " <<arr[3]<<" " <<arr[4]<<" " <<arr[5]<<" ";
+
+// o/p ----> values ye hai 1 23 45 556 667 66 
+
+
+
+
+return 0;
+}
