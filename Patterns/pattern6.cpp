9e187bcf5bd9e8044c0a27e89273534aba24c66a
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-24 11:40:02 +0530

    Pyramid pattern

---
+++ Patterns/pattern6.cpp
@@ -0,0 +1,41 @@
+// #include<iostream>
+// using namespace std;
+
+
+// void printReversePyramid(int n){
+//     for (int i = 1; i < n; i++)
+//     {
+
+//       // space 
+//         for(int j=0;j<=i;j++){
+//                 cout<<" ";     
+//         }
+       
+//          //   star 
+//         for(int j=0;j<=2*n-(2*i+1);j++){
+//                 cout<<"*";     
+//         }
+
+//          // space 
+//         for(int j=0;j<=i;j++){
+//                 cout<<" "; 
+
+        
+//     }
+//     cout<<endl;
+
+    
+// }
+// }
+
+// int main(){
+
+//     int n;
+//     cout<<"enter the value: ";
+//     cin>>n;
+//     printReversePyramid (n);
+
+
+
+// return 0;
+// }
