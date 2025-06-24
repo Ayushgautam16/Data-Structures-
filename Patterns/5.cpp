987a670806c1ad92cdee5aed3cfd7592c7944ceb
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-24 11:15:29 +0530

    pattern

--- Patterns/5.cpp
+++ Patterns/5.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
+
+void printPyramid(int n){
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        
+    }
+    
+}
+
 int main(){
 
 return 0;
