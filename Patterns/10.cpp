b0848eead2528bb35fae6f6603271e7ef2d19ab8
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-24 16:31:01 +0530

    pattern

--- Patterns/10.cpp
+++ Patterns/10.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+void print10(int n ){
+    for(int i=1;i<=n;i++) {
+
+         for(int j=1;j<=i;j++){  
+         cout<<j;
+        }
+         for(int j =1; j<=i; j++){
+        cout<<j;
+         }
+    }
+   
+
+}
 int main(){
 
 return 0;
