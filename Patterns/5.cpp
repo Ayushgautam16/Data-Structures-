5d881feca0909f10b3ad11c28bd73a293fc8f76d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-24 11:29:13 +0530

    pattern6

--- Patterns/5.cpp
+++ Patterns/5.cpp
@@ -1,16 +1,66 @@
 #include<iostream>
 using namespace std;
 
+
+// heare we need the three things such as ------> 
+
+// outer loop for the rows   ------->1
+
+// space                     ------->2
+// n-i-1
+
+// star                      ------->3
+// 2i+1
+
+// space again               ------->4
+// n-i-1
+
 void printPyramid(int n){
     for (int i = 0; i < n; i++)
     {
-        /* code */
+        // space 
+        for(int j=0;j<=n-i-1;j++){
+                cout<<" ";     
+        }
+       
+         //   star 
+        for(int j=0;j<=2*i+1;j++){
+                cout<<"*";     
+        }
+
+         // space 
+        for(int j=0;j<=n-i-1;j++){
+                cout<<" "; 
+
         
     }
+    cout<<endl;
+
     
+}
 }
 
 int main(){
 
+    int n;
+    cout<<"enter the value: ";
+    cin>>n;
+    printPyramid (n);
+
+
+
 return 0;
 }
+
+
+// enter the value: 10
+//           **
+//          ****
+//         ******        
+//        ********
+//       **********
+//      ************     
+//     **************
+//    ****************   
+//   ******************
+//  ********************
