dddcc40fed9a970ce7d162b6c5bbd99cb8da432e
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-26 22:24:34 +0530

    1

--- C++ STL/vector.cpp
+++ C++ STL/vector.cpp
@@ -1,13 +1,38 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void printVec(vector<int>v){
+//     cout<<"size"<<v.size()<<endl;
+//     for(int i =0; i<v.size(); i++){
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main(){
+// vector<int> v;
+// int n ;
+// cin>>n;
+// for(int i=0;i<n;i++){
+//     int s;
+//     cin>>s;
+//     v.push_back(s);
+
+    
+// }
+// printVec(v);
+// return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-vector<int> v;
-int n ;
-cin>>n;
-for(int i=0;i<n;i++){
-    int s;
-    cin>>s;
-    v.push_back(s);
+vector<int,string>v ={13,334,5,};
+for(int num:v){
+    cout<<num ;
+     
 }
+
+
 return 0;
 }
