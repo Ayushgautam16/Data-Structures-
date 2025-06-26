3da7b3b9ea00d69cbee5d867e60d6ad5cd4637f7
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-06-26 22:28:05 +0530

    1

--- Arrays/arr1.cpp
+++ Arrays/arr1.cpp
@@ -6,7 +6,7 @@ cin >>arr[0]>>arr[1] >>arr[2] >>arr[3] >>arr[4] >>arr[5];
 
 cout<<"values ye hai ";
 
-cout <<arr[0]<<" "<<arr[1]<<" " <<arr[2]<<" " <<arr[3]<<" " <<arr[4]<<" " <<arr[5]<<" ";
+// cout <<arr[0]<<" "<<arr[1]<<" " <<arr[2]<<" " <<arr[3]<<" " <<arr[4]<<" " <<arr[5]<<" ";
 
 // o/p ----> values ye hai 1 23 45 556 667 66 
 
