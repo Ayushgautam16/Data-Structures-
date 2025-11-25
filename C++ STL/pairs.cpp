37209db1adf3a4c04d22a22cf502dc48d6a88e5e
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2025-11-25 21:45:36 +0530

    Update pairs.cpp

--- C++ STL/pairs.cpp
+++ C++ STL/pairs.cpp
@@ -42,9 +42,9 @@ cout<<p.first<<" "<<p.second<<endl;
 int a[]={1,23,4};
 int b[]={1,23,4};
 
-pair<int,int> p_array[3];
- p_array[3] ={1,2};
- p_array[3] ={1,2};
+// pair<int,int> p_array[3];
+//  p_array[3] ={1,2};
+//  p_array[3] ={1,2};
  // p_array[3] ={1,2};
  // p_array[3] ={1,2};
  
