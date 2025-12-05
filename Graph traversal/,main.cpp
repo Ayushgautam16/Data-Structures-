cef678abdba6fd25f7948d2a034d2c67a0818196
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2025-12-05 12:09:23 +0530

    Update ,main.cpp

--- Graph traversal/,main.cpp
+++ Graph traversal/,main.cpp
@@ -1,7 +1,8 @@
-class Solution {
-public:
-    int countPartitions(vector<int>& A) {
-        int total = accumulate(A.begin(), A.end(), 0);
-        return total & 1 ? 0 : A.size() - 1;
-    }
-};
+// class Solution {
+// public:
+//     int countPartitions(vector<int>& A) {
+//         int total = accumulate(A.begin(), A.end(), 0);
+//         return total & 1 ? 0 : A.size() - 1;
+//     }
+// };
+
