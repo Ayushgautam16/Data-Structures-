f1ee46378c6c81a7cdb517b599b1110b0685cbab
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2026-02-25 08:10:37 +0530

    Fix formatting issue in hasAlternatingBits method

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean hasAlternatingBits(int n) {
         int prev = 5;   
         while(n > 0){
-            int bit = n % 2;   // get last bit
+            int bit = n % 2;   
             n /= 2;            // remove last bit
             if(prev == 5){
                 prev = bit;    // first bit, just store it
