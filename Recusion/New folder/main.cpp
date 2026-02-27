f070cc59b4e6ae3e09a5fb2d6d6d8c16b94b3f4e
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2026-02-27 11:35:54 +0530

    Update main.cpp

--- Recusion/New folder/main.cpp
+++ Recusion/New folder/main.cpp
@@ -4,4 +4,17 @@ public:
         int total = accumulate(A.begin(), A.end(), 0);
         return total & 1 ? 0 : A.size() - 1;
     }
+
 };
+
+
+class Solution {
+public:
+    int minOperations(string s, int k) {
+        int n = s.length();
+        int Z = 0;
+        for (char c : s) if (c == '0') Z++;
+        if (Z == 0) return 0;
+
+        for (long long t = (Z + k - 1) / k; t <= n; ++t) {
+            long long total_needed = t * k;
